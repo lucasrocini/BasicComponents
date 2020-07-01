609b88ed695bf715bdc3bffedfef2ec5b1db4183
Lucas <lucasrocini@hotmail.com>
2020-06-30 23:27:20 -0300

    a

--- app/src/main/java/com/example/basiccomponents/MainActivity.java
+++ app/src/main/java/com/example/basiccomponents/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.basiccomponents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enviar(View view){
+
+        EditText campoNome = findViewById(R.id.editNome);
+        TextInputEditText campoEmail = findViewById(R.id.editEmail);
+        TextView textoResultado = findViewById(R.id.textResultado);
+
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+        textoResultado.setText("nome: " + nome + " / email: " + email);
+
+
     }
 }
